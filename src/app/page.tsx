aa8bc37dbc7a65e3c5589d908a739d764532d2dc
Kwesi Hervie <herviek2001@gmail.com>
2024-01-26 18:42:31 +0100

    updates

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,8 +7,8 @@ import React from "react";
 import { time } from "console";
 
 export default function Home() {
-  const [vantaEffect, setVantaEffect] = React.useState(0);
-  const vantaRef = React.useRef(null);
+  const [vantaEffect, setVantaEffect] = React.useState<any>(0);
+  const vantaRef: any = React.useRef(null);
 
   React.useEffect(() => {
     if (!vantaEffect) {
@@ -26,16 +26,26 @@ export default function Home() {
       );
     }
     return () => {
-      if (vantaEffect) vantaEffect.destroy();
+      if (vantaEffect) vantaEffect?.destroy();
     };
   }, [vantaEffect]);
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-between">
       <div ref={vantaRef} className="w-screen h-screen bg-white100">
-        <p style={{ color: "#fff", paddingTop: "20px" }}></p>
+        <div className="w-full flex justify-end">
+          <h2 className="text-[200px] font-bold text-white bg-gradient-to-r from-blue-600 via-green-500 to-indigo-400 inline-block text-transparent bg-clip-text">
+            Brand <br />
+            Brains
+          </h2>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum,
+            temporibus hic nam a quasi numquam, vel dignissimos ad inventore
+            consectetur quo ut eligendi sapiente tenetur culpa modi facere
+            ducimus perferendis.
+          </p>
+        </div>
       </div>
-      <div className="h-screen w-screen bg-green-600"></div>
     </main>
   );
 }
